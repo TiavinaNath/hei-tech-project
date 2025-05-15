a4bb1a575ebcfdb5cccc2f0b67e20097664dbd6a
ArenaMendrika <hei.arena.3@gmail.com>
2025-05-15 17:45:04 +0300

    fix: fix requestHeader

---
+++ src/components/features/client-dashboard/InRequestHeader.tsx
@@ -0,0 +1,49 @@
+import { format } from 'date-fns'
+import { MapPin, CalendarDays } from 'lucide-react'
+import styles from '@/app/style/InrequestHeader.module.css'
+
+export default function InRequestHeader({ request }: { request: any }) {
+	return (
+		<div className={styles.card}>
+			<div className={styles.content}>
+				{/* Remplace p par div ici */}
+				<div className={styles.para}>
+					<strong className="text-lg relative inline-block text-[#457bed]">
+						{request.title}
+						<span className="block w-70 h-0.5 bg-[#457bed] mt-1 mx-auto rounded-full"></span>
+					</strong>
+
+					<div className="mt-4 space-y-1">
+						<div>
+							{request.description}
+							<span className="block w-70 h-0.5 mt-1 mx-auto rounded-full"></span>
+						</div>
+
+						<div>
+							<div className="flex items-center gap-2">
+								<div className="bg-[#457bed] rounded-full p-1 text-white">
+									<MapPin size={22} />
+								</div>
+								{request.address_text}
+							</div>
+
+							<div className="flex items-center gap-2 mt-2">
+								<div className="bg-[#457bed] rounded-full p-1 text-white">
+									<CalendarDays
+										size={22}
+										className="flex items-center gap-2"
+									/>
+								</div>
+								{format(new Date(request.preferred_date_time), 'EEEE dd MMMM yyyy à HH:mm')}
+							</div>
+
+							<div className={styles.serviceTag}>
+								{request.services?.name || 'Service'}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
